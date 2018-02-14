ef21d71bb865f39f7bae264e54e1a965e614024b
Alex <alex.g@spotinst.com>
2018-02-14 15:41:16 -0800

    no more emails

--- tests/tests.js
+++ tests/tests.js
@@ -3,17 +3,13 @@ const deployIndex = require('../deploy/index.js');
 const SpotinstObj = require('./utils/SpotinstServerlessObject');
 const Serverless = require('serverless');
 
-
-
 // describe a test group
-describe("deploy/index.js tests", () => {
+describe("logs/index.js tests", () => {
   // describe a specific test
-  it('create', (done) => {
-    chai.assert.equal(1,1);
-    console.log("in the create test");
+  it('logs()', (done) => {
     let serverless = new Serverless();
-    console.log(serverless);
-    // will return pass/fail with optional message (can be output to logs if you wanna be like that)
+    
+    
     done();
   });
 });
