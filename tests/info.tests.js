d3689f240440dcad4495091b25ba81c25be164d3
jeffnoehren <jeffrey@spotintst.com>
2018-02-14 16:30:22 -0800

    testing info/info()

--- tests/info.tests.js
+++ tests/info.tests.js
@@ -4,14 +4,14 @@ const Serverless = require('serverless');
 const config = require('../config.js');
 
 const serverless = new Serverless();
-const infoObject = new InfoObject(serverless, {})
+const infoObject = new InfoObject(serverless, { stage: 'dev', region: "here" })
 
 // describe a test group
 describe("info/index.js tests", () => {
   // describe a specific test
-  it('init()', (done) => {
-  	console.log(config.provider)
-  	console.log(infoObject.init)    
+  it('init() test', (done) => {
+  	console.log(config.providerName)
+  	console.log(infoObject.info())    
     
     done();
   });
